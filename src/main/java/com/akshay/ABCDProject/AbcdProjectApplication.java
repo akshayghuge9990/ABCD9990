039a76d3e9913611d5b8b0ccd453fc56bb045d1c
akshayghuge9990 <ghugeakshay123@gmail.com>
2023-01-29 17:51:34 +0530

    Changes by Akshay Ghuge

---
+++ src/main/java/com/akshay/ABCDProject/AbcdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.akshay.ABCDProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcdProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akshay/ABCDProject/AbcdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay.ABCDProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
